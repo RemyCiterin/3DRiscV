10b0c932d511e018117514e2c24ded0142b26127
remy <remyciterin@gmail.com>
2025-06-23 12:07:34 +0200

    add probe in CoherentBCache

--- src/TileLink/CoherentBCache.hs
+++ src/TileLink/CoherentBCache.hs
@@ -18,6 +18,7 @@ import Blarney.ADT
 import TileLink.Types
 import TileLink.Utils
 import TileLink.AcquireRelease
+import Utils
 
 storeListRAM :: (Bits a, Bits b, KnownNat idx) => [RAM a b] -> Bit idx -> a -> b -> Action ()
 storeListRAM rams index k v =
@@ -62,11 +63,44 @@ data BCacheCore kw iw ow atomic p =
     , atomicResponse :: Source (Bit (8*LaneWidth p)) }
 
 type BCacheState = Bit 3
-st_idle, st_lookup, st_acquire, st_release :: BCacheState
+st_idle, st_lookup, st_acquire, st_release, st_probe_lookup, st_probe_burst :: BCacheState
 st_idle = 0
 st_lookup= 1
 st_acquire= 2
 st_release= 3
+st_probe_lookup= 4
+st_probe_burst=5
+
+encode :: forall p kw iw ow.
+  (KnownNat kw
+  , KnownNat iw
+  , KnownNat ow
+  , KnownTLParams p
+  , KnownNat (Log2 (LaneWidth p))
+  , KnownNat (kw+(iw+(ow+Log2 (LaneWidth p)))))
+  => Bit kw
+  -> Bit iw
+  -> Bit ow
+  -> Bit (AddrWidth p)
+encode key index offset =
+  cast (key # index # offset # (0 :: Bit (Log2 (LaneWidth p))))
+
+decode :: forall p kw iw ow.
+  (KnownNat kw
+  , KnownNat iw
+  , KnownNat ow
+  , KnownTLParams p
+  , KnownNat (Log2 (LaneWidth p)))
+  => Bit (AddrWidth p)
+  -> (Bit kw, Bit iw, Bit ow)
+decode addr =(key, index, offset)
+  where
+    key = truncateLSBCast addr
+    index = unsafeSlice (startKey-1,startIndex) addr
+    offset = unsafeSlice (startIndex-1,startOffset) addr
+    startOffset = valueOf @(Log2 (LaneWidth p))
+    startIndex = startOffset + valueOf @ow
+    startKey = startIndex + valueOf @iw
 
 makeBCacheCoreWith :: forall w kw iw ow atomic p.
   ( Bits atomic
@@ -93,6 +127,7 @@ makeBCacheCoreWith source slave execAtomic = do
   scResponseQ :: Queue (Bit 1) <- makeQueue
 
   state :: Ehr BCacheState <- makeEhr 2 st_idle
+  nextState :: Reg BCacheState <- makeReg dontCare
 
   let ways = 2^(valueOf @w)
   permRam :: [RAM (Bit iw) TLPerm] <- replicateM ways makeDualRAMForward
@@ -113,6 +148,7 @@ makeBCacheCoreWith source slave execAtomic = do
   index :: Reg (Bit iw) <- makeReg dontCare
   key :: Reg (Bit kw) <- makeReg dontCare
   way :: Reg (Bit w) <- makeReg dontCare
+  cap :: Reg TLPerm <- makeReg dontCare
 
   let execOp :: Bit w -> Action () = \ way -> do
         reserved <== request.val `isTagged` #LoadR
@@ -132,13 +168,13 @@ makeBCacheCoreWith source slave execAtomic = do
             let (mask,lane) = untag #Store request.val
             dataRamA.storeBE (way # index.val # offset.val) mask lane
 
-  let address :: Bit kw -> Bit (AddrWidth p) = \ key ->
-        cast (key # index.val # (0 :: Bit ow) # (0 :: Bit (Log2 (LaneWidth p))))
+  let baseAddr :: Bit kw -> Bit (AddrWidth p) = \ key ->
+        encode @p @kw @iw @ow key index.val 0
 
   always do
     when (state.read 0 === st_release .&&. releaseM.ack.canPeek .&&. acquireM.canAcquire) do
-      acquireM.acquireBlock (tag #NtoT ()) (way.val # index.val # 0) (address key.val)
-      --display "acquire block: 0x" (formatHex 0 (address key.val))
+      acquireM.acquireBlock (tag #NtoT ()) (way.val # index.val # 0) (baseAddr key.val)
+      --display "acquire block: 0x" (formatHex 0 (baseAddr key.val))
       state.write 0 st_acquire
       releaseM.ack.consume
 
@@ -150,9 +186,62 @@ makeBCacheCoreWith source slave execAtomic = do
       perm <- acquireM.acquireAck
       storeListRAM permRam way.val index.val (dataRamA.storeActiveBE ? (dirty,perm))
 
+  always do
+    when ((state.read 1 === st_idle .||. state.read 1 === st_acquire) .&&. probeM.start.canPeek) do
+      let (addr, perm) = probeM.start.peek
+      let (k,idx,off) = decode @p addr
+      state.write 1 st_probe_lookup
+      nextState <== state.read 1
+
+      key <== k
+      index <== idx
+      offset <== off
+      sequence_ [do
+          p.load idx
+          t.load idx
+          | (p,t) <- zip permRam keyRam]
+      cap <== perm
+
+    when (state.read 0 === st_probe_lookup .&&. probeM.evict.canPut) do
+      state.write 0 st_probe_burst
+
+      let hit =
+            orList
+              [key.val === t.out .&&. p.out .>. nothing
+                | (t,p) <- zip keyRam permRam]
+
+      let (way :: Bit w, perm) =
+            selectDefault (0, nothing)
+              [key.val === t.out .&&. p.out .>. nothing --> (constant i, p.out)
+                | (t,p,i) <- zip3 keyRam permRam [0..]]
+
+      when (perm === nothing) do
+        probeM.evict.put (n2n, none)
+      when (perm === branch) do
+        if cap.val === nothing then do
+          probeM.evict.put (b2n, none)
+          storeListRAM permRam way index.val nothing
+        else do
+          probeM.evict.put (b2b, none)
+      when (perm .>=. trunk) do
+        when (cap.val === trunk) do
+          probeM.evict.put (t2t, none)
+        when (cap.val === branch) do
+          probeM.evict.put (t2b, some $ way # index.val # 0)
+          storeListRAM permRam way index.val branch
+          reserved <== false
+        when (cap.val === nothing) do
+          probeM.evict.put (t2n, some $ way # index.val # 0)
+          storeListRAM permRam way index.val nothing
+          reserved <== false
+
+    when (state.read 0 === st_probe_burst .&&. probeM.ack.canPeek) do
+      state.write 0 nextState.val
+      probeM.ack.consume
+
   return
     BCacheCore
-      { canLookup= state.read 1 === st_idle
+      { canLookup= state.read 1 === st_idle .&&. inv probeM.start.canPeek
       , lookup= \ idx off req -> do
           dynamicAssert (state.read 1 === st_idle) "lookup with an unexpected state"
           request <== req
@@ -181,35 +270,40 @@ makeBCacheCoreWith source slave execAtomic = do
                   [msb === t.out .&&. satisfyPerms @atomic @p request.val p.out
                     | (t,p) <- zip keyRam permRam]
 
-          if hit then do
-            let way :: Bit w =
-                  select
-                    [msb === t.out --> constant i
-                      | (t,i) <- zip keyRam [0..]]
+          let hitWay :: Bit w =
+                selectDefault randomWay.val
+                  [msb === t.out .&&. p.out .>. nothing --> constant i
+                    | (t,p,i) <- zip3 keyRam permRam [0..]]
 
-            execOp way
+          if hit then do
+            execOp hitWay
             state.write 0 st_idle
             when (dataRamA.storeActiveBE) do
-              storeListRAM permRam way index.val dirty
+              storeListRAM permRam hitWay index.val dirty
           else do
+            way <== hitWay
             reserved <== false
-            way <== randomWay.val
-            storeListRAM permRam randomWay.val index.val nothing
-            if outListRAM permRam randomWay.val =!= nothing then do
+            if outListRAM permRam hitWay =!= nothing .&&. outListRAM keyRam hitWay =!= msb then do
+              let oldPerm = outListRAM permRam hitWay
+              let oldKey = outListRAM keyRam hitWay
+
+              -- writeback only if the choosen block is dirty
+              when (oldPerm === dirty) do
+                releaseM.start.put (t2n, baseAddr oldKey, some $ hitWay # index.val # 0)
+              when (oldPerm === trunk) do
+                releaseM.start.put (t2n, baseAddr oldKey, none)
+              when (oldPerm === branch) do
+                releaseM.start.put (b2n, baseAddr oldKey, none)
+
+              -- invalidate the permission of the choosen block
+              storeListRAM permRam hitWay index.val nothing
               state.write 0 st_release
-              let key = outListRAM keyRam randomWay.val
-              when (outListRAM permRam randomWay.val === dirty) do
-                releaseM.start.put (t2n, address key, some $ randomWay.val # index.val # 0)
-              when (outListRAM permRam randomWay.val === trunk) do
-                releaseM.start.put (t2n, address key, none)
-              when (outListRAM permRam randomWay.val === branch) do
-                releaseM.start.put (b2n, address key, none)
             else do
               state.write 0 st_acquire
               acquireM.acquireBlock
                 (needPermTrunk @atomic @p request.val ? (n2t, n2b))
-                (randomWay.val # index.val # 0)
-                (address msb)
+                (hitWay # index.val # 0)
+                (baseAddr msb)
 
           return ()
       , abort= state.write 0 st_idle
@@ -243,22 +337,25 @@ makeBCacheCore :: forall w kw iw ow atomic p.
     -> Module (BCacheCore kw iw ow atomic p, TLMaster p)
 makeBCacheCore source execAtomic = do
   queueA <- makeQueue
+  queueB <- makeQueue
+  queueC <- makeQueue
   queueD <- makeQueue
+  queueE <- makeQueue
 
   let slave =
         TLSlave
           { channelA= toSink queueA
-          , channelB= nullSource
-          , channelC= nullSink
+          , channelB= toSource queueB
+          , channelC= toSink queueC
           , channelD= toSource queueD
-          , channelE= nullSink }
+          , channelE= toSink queueE }
   let master =
         TLMaster
           { channelA= toSource queueA
-          , channelB= nullSink
-          , channelC= nullSource
+          , channelB= toSink queueB
+          , channelC= toSource queueC
           , channelD= toSink queueD
-          , channelE= nullSource }
+          , channelE= toSource queueE }
 
   ifc <- makeBCacheCoreWith @w @kw @iw @ow @atomic @p source slave execAtomic
   return (ifc,master)
